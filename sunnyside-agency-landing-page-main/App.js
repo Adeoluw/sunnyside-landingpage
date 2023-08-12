31c6ace251081ac99cdf25d86865b72d765c05e7
Adeolu <afolabiadeolu36@gmail.com>
2023-08-12 17:22:09 +0100

    updating...

--- sunnyside-agency-landing-page-main/App.js
+++ sunnyside-agency-landing-page-main/App.js
@@ -11,8 +11,11 @@ openMenu.addEventListener("click", () => {
     if (nav.style.opacity === "0") {
         nav.style.opacity = "1";
         nav.style.zIndex = "999";
+        // nav.style.animationName = "navination";
+        nav.style.transform = "translateY(20px)"
     } else {
         nav.style.opacity = "0";
+        nav.style.transform = "translateY(-20px)";
         nav.style.zIndex = "0";  
     }
 
